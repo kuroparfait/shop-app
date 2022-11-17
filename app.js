80daae7d53804fc402b07c1bfca68a81d3594f1c
kuroparfait <kuroparfait.9696@gmail.com>
2022-11-17 14:38:41 +0900

    2022-11-17

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const http = require('http');
+/*const fs = require('fs');
+const http = require('http');*/
+const ejs = require('ejs');
+const express = require('express');
+var app = express();
 
-var server = http.createServer(
-    (req,res)=>{
-        res.end('Hello World');
-    }
-);
+//const cat1_page = fs.readFileSync('./cat1.ejs','utf-8');
+//const cat2_page = fs.readFileSync('./cat2.ejs','utf-8');
 
-server.listen(3000);
+//var server = http.createServer(getFromClient);
+
+app.listen(3000,()=>{
+    console.log('Start server port:3000')
+})
+
+// ここまでメインプログラム ＝＝＝＝＝＝
+
+// CreateServerの処理
+app.get('/',(req,res) => {
+    var content = ejs.render(thePages)
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write(content);
+    res.end();
+})
---
+++ zaiko/db.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.render()
+})
+
+function db(){
+
+}
+
+function dbedit(){
+
+}
+
+function dbarr(){
+    
+}
+
+function dbnew(){
+
+}
